eac26a798f9553f7b108e5b3e37a1921700dc0ac
Wellington Junior <wtc-jr@outlook.com>
2023-07-05 16:46:27 -0300

    07.2023_TDD_BDD_library-api: Using Bean Validation and Exception Handler Resources

--- 07.2023_TDD_BDD_library-api/src/main/java/com/cursosudemy/libraryapi/api/resources/BookController.java
+++ 07.2023_TDD_BDD_library-api/src/main/java/com/cursosudemy/libraryapi/api/resources/BookController.java
@@ -1,11 +1,18 @@
 package com.cursosudemy.libraryapi.api.resources;
 
 import com.cursosudemy.libraryapi.api.dto.BookDTO;
+import com.cursosudemy.libraryapi.api.exceptions.ApiErrors;
 import com.cursosudemy.libraryapi.model.entity.Book;
 import com.cursosudemy.libraryapi.service.BookService;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
@@ -21,13 +28,22 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDTO create(@RequestBody BookDTO dto) {
+    public BookDTO create(@RequestBody @Valid BookDTO dto) {
         //Convert DTO to Entity
         Book entity = modelMapper.map(dto, Book.class);
 
         entity = service.save(entity);
 
         return modelMapper.map(entity, BookDTO.class);
+    }
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidationExceptions(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+
+        return new ApiErrors(bindingResult);
+
     }
 
 }
--- 07.2023_TDD_BDD_library-api/src/test/java/com/cursosudemy/libraryapi/api/resource/BookControllerTest.java
+++ 07.2023_TDD_BDD_library-api/src/test/java/com/cursosudemy/libraryapi/api/resource/BookControllerTest.java
@@ -3,7 +3,10 @@ package com.cursosudemy.libraryapi.api.resource;
 import com.cursosudemy.libraryapi.api.dto.BookDTO;
 import com.cursosudemy.libraryapi.model.entity.Book;
 import com.cursosudemy.libraryapi.service.BookService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -69,9 +72,22 @@ public class BookControllerTest {
         ;
     }
 
+    @Test
     @DisplayName("Deve lançar erro de validação quando não houver dados sufiente para criação do livro..")
-    public void createInvalidBookTest () {
+    public void createInvalidBookTest () throws Exception {
+        String json = new ObjectMapper().writeValueAsString(new BookDTO());
 
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("errors", Matchers.hasSize(3)))
+
+        ;
     }
 
 }

1f7daef2d9f5b10749f1063d7f241890d2b059b8
WellJr <wellington.pessoal@SND18798DAW10P.sonda.com>
2023-12-26 15:21:18 -0300

    12.2023_TDD_BDD_library-api: Class 76. Implementing Loan Integration Test (repository)

--- 07.2023_TDD_BDD_library-api/src/test/java/com/cursosudemy/libraryapi/model/repository/LoanRepositoryTest.java
+++ 07.2023_TDD_BDD_library-api/src/test/java/com/cursosudemy/libraryapi/model/repository/LoanRepositoryTest.java
@@ -2,12 +2,15 @@ package com.cursosudemy.libraryapi.model.repository;
 
 import com.cursosudemy.libraryapi.model.entity.Book;
 import com.cursosudemy.libraryapi.model.entity.Loan;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -54,6 +57,32 @@ public class LoanRepositoryTest {
 
         //vefificação
         assertThat(exists).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve buscar emprestimo pelo isbn do livro ou customer")
+    public void findByBookIsbnOrCustomerTest() {
+        //cenário
+        Book book = createNewBook("123");
+        entityManager.persist(book);
+
+        Loan loan = Loan.builder()
+                .book(book)
+                .customer("Fulano")
+                .loanDate(LocalDate.now())
+                .build();
+
+        entityManager.persist(loan);
+
+        //ação
+        Page<Loan> result = repository.findByBookIsbnOrCustomer("123", "Fulano", PageRequest.of(0, 10));
+
+        //verificação
+        Assertions.assertThat(result.getContent()).hasSize(1);
+        Assertions.assertThat(result.getContent()).contains(loan);
+        Assertions.assertThat(result.getPageable().getPageSize()).isEqualTo(10);
+        Assertions.assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
+        Assertions.assertThat(result.getTotalElements()).isEqualTo(1);
     }
 
     private Book createNewBook(String isbn) {

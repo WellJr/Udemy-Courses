1a3fa62669aff722e8c9f6bfa9dfc14b2e63139f
Wellington Junior <wtc-jr@outlook.com>
2024-04-04 22:27:39 -0300

    04.2024_TDD_BDD_library-api: Adding Spring Boot Admin

---
+++ spring-boot-admin/src/main/java/com/cursosudemy/SpringBootAdmin.java
@@ -0,0 +1,14 @@
+package com.cursosudemy;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdmin {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdmin.class, args);
+    }
+
+}

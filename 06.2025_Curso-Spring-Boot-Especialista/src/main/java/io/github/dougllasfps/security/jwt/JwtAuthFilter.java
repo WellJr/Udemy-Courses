d94b9509a63cb3bc02f94bd27c2e44bb05624965
Wellington Junior <wtc-jr@outlook.com>
2025-06-20 13:27:04 -0300

    06.2025_Spring_Security: Implementing - JWT Filter

---
+++ 06.2025_Curso-Spring-Boot-Especialista/src/main/java/io/github/dougllasfps/security/jwt/JwtAuthFilter.java
@@ -0,0 +1,58 @@
+package io.github.dougllasfps.security.jwt;
+
+import io.github.dougllasfps.service.impl.UserDetailServiceImpl;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private JwtService jwtService;
+
+    private UserDetailServiceImpl userDetailService;
+
+    public JwtAuthFilter(JwtService jwtService, UserDetailServiceImpl userDetailService) {
+        this.jwtService = jwtService;
+        this.userDetailService = userDetailService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String authorization = httpServletRequest.getHeader("Authorization");
+
+        if(authorization != null && authorization.startsWith("Bearer")) {
+            String token = authorization.split(" ")[1];
+            boolean isValid = jwtService.isTokenValido(token);
+
+            if(isValid) {
+                String loginUsuario = jwtService.obterLoginUsuario(token);
+                UserDetails userDetails = userDetailService.loadUserByUsername(loginUsuario);
+
+                // coloca usuario no contexto do spring security
+                //UsernamePasswordAuthenticationToken -> Serve para informar que a autenticação é uma autenticação web
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource()
+                        .buildDetails(httpServletRequest));
+
+                //Contexto do Spring Security
+                // Serve para dizer quem é ou pegar o usuário autenticadoo
+                SecurityContextHolder.
+                        getContext()
+                        .setAuthentication(usernamePasswordAuthenticationToken);
+            }
+
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        }
+    }
+}

eda64617348f8f76660950362ce29415263a60bd
Wellington Junior <wtc-jr@outlook.com>
2025-06-26 21:50:24 -0300

    06.2025_Spring_Security_6: Step #1 (Basic Auth) - Creating users structure for database

---
+++ 06.2025_libraryapi_spring_secirity/src/main/java/io/github/cursodsousa/libraryapi/model/Usuario.java
@@ -0,0 +1,31 @@
+package io.github.cursodsousa.libraryapi.model;
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column
+    private String login;
+
+    @Column
+    private String senha;
+
+    @Type(ListArrayType.class) //translate list to array
+    @Column(name = "roles", columnDefinition = "varchar[]")
+    private List<String> roles;
+
+}

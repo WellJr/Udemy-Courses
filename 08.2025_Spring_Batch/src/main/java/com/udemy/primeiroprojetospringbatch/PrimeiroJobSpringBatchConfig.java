bc34648c94f970ff6b12472229f597173e40b7fb
Wellington Junior <wtc-jr@outlook.com>
2025-08-25 20:54:09 -0300

    08.2025_Spring_Batch: Multiple execution and scope

--- 08.2025_Spring_Batch/src/main/java/com/udemy/primeiroprojetospringbatch/PrimeiroJobSpringBatchConfig.java
+++ 08.2025_Spring_Batch/src/main/java/com/udemy/primeiroprojetospringbatch/PrimeiroJobSpringBatchConfig.java
@@ -6,11 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -35,14 +37,20 @@ public class PrimeiroJobSpringBatchConfig {
 	public Step imprimeOlaStep() {
 		return stepBuilderFactory
 				.get("imprimeOlaStep")
-				.tasklet(new Tasklet() {
-					
-					@Override
-					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-						System.out.println("Olá, mundo!");
-						return RepeatStatus.FINISHED;
-					}
-				})
+				.tasklet(imprimeOlaTasklet(null))
 				.build();
+	}
+
+	@Bean
+	@StepScope //anotação necessária para que possa ser pego o parâmetro da task
+	public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']}") String nome) {
+		return new Tasklet() {
+
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println(String.format("Olá, %s!", nome));
+				return RepeatStatus.FINISHED;
+			}
+		};
 	}
 }

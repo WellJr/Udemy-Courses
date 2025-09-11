346c2c92df329299548e2e7ce7da30dd5ce9c45f
Wellington Junior <wtc-jr@outlook.com>
2025-09-11 20:38:24 -0300

    08.2025_Spring_Batch: Processors of validation

---
+++ 08.2025_Spring_Batch/ProcessadorValidacaoJob/src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
@@ -0,0 +1,47 @@
+package com.springbatch.processadorvalidacao.processor;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.validator.BeanValidatingItemProcessor;
+import org.springframework.batch.item.validator.ValidatingItemProcessor;
+import org.springframework.batch.item.validator.ValidationException;
+import org.springframework.batch.item.validator.Validator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springbatch.processadorvalidacao.dominio.Cliente;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+public class ProcessadorValidacaoProcessorConfig {
+
+	private Set<String> emails = new HashSet<>();
+
+	@Bean
+	public ItemProcessor<Cliente, Cliente> procesadorValidacaoProcessor() {
+//		BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor<Cliente>();
+//
+//		// Filtra os dados e impede que o processamento pare.
+//		processor.setFilter(true);
+
+		// Cria validação personalizada
+		ValidatingItemProcessor<Cliente> processor = new ValidatingItemProcessor<Cliente>();
+		processor.setValidator(validator());
+		processor.setFilter(true); //<-- Não interrompe a execução
+
+		return processor;
+	}
+
+	private Validator<Cliente> validator() {
+		return new Validator<Cliente>() {
+			@Override
+			public void validate(Cliente cliente) throws ValidationException {
+				if(emails.contains(cliente.getEmail())) {
+					throw new ValidationException(String.format("O cliente %s já foi processado!", cliente.getEmail()));
+				}
+				emails.add(cliente.getEmail());
+			}
+		};
+	}
+}

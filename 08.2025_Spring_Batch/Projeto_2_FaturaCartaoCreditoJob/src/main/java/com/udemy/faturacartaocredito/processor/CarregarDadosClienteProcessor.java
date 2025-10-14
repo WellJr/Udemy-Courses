32c1e108f78aab6e53ae1b4b5e22443d081ffd79
Wellington Junior <wtc-jr@outlook.com>
2025-10-14 20:58:00 -0300

    08.2025_Spring_Batch: Project #2: Processing to enrich the date from client

---
+++ 08.2025_Spring_Batch/Projeto_2_FaturaCartaoCreditoJob/src/main/java/com/udemy/faturacartaocredito/processor/CarregarDadosClienteProcessor.java
@@ -0,0 +1,28 @@
+package com.udemy.faturacartaocredito.processor;
+
+import com.udemy.faturacartaocredito.dominio.Cliente;
+import com.udemy.faturacartaocredito.dominio.FaturaCartaoCredito;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.validator.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class CarregarDadosClienteProcessor implements ItemProcessor<FaturaCartaoCredito, FaturaCartaoCredito> {
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Override
+    public FaturaCartaoCredito process(FaturaCartaoCredito faturaCartaoCredito) throws Exception {
+        String uri = String.format("https://my-json-server.typicode.com/giuliana-bezerra/demo/profile/%d", faturaCartaoCredito.getCliente().getId());
+        ResponseEntity<Cliente> response = restTemplate.getForEntity(uri, Cliente.class);
+
+        if(response.getStatusCode() != HttpStatus.OK) {
+            throw new ValidationException("Cliente não encontrado");
+        }
+
+        faturaCartaoCredito.setCliente(response.getBody());
+        return faturaCartaoCredito;
+    }
+}

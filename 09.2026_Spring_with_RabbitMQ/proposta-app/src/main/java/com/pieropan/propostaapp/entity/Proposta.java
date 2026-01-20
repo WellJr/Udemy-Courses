a768f86fdfedce21e7925c3da0941cbd05d54ce9
Wellington Junior <wtc-jr@outlook.com>
2026-01-19 21:31:05 -0300

    01.2026_Spring_with_RabbitMQ: Implementing CascadeType.PERSIST in Proposal Entity

--- 09.2026_Spring_with_RabbitMQ/proposta-app/src/main/java/com/pieropan/propostaapp/entity/Proposta.java
+++ 09.2026_Spring_with_RabbitMQ/proposta-app/src/main/java/com/pieropan/propostaapp/entity/Proposta.java
@@ -1,12 +1,6 @@
 package com.pieropan.propostaapp.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,7 +28,7 @@ public class Proposta {
 
     private String observacao;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.PERSIST) // Salva o objeto usuário caso ainda não exista
     @JoinColumn(name = "id_usuario")
     private Usuario usuario;
 
